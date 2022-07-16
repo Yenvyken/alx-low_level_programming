e0f954b5c115dad48e25317bc82af25f3f4e0232
Yenvyken <Yenvyken>
2022-07-16 15:44:28 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,7 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -5,9 +9,10 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-int _putchar(char c);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+
+#endif
